59be0ac8115619938a2407c8c09e50ebbba41fa2
Berkay-Disli <berkaydisli.work@gmail.com>
2022-10-14 15:32:59 +0300

    Initial Commit

---
+++ NFTApp/NFTAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NFTAppApp.swift
+//  NFTApp
+//
+//  Created by Berkay Disli on 14.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct NFTAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
